871ebee0eb0657f20aca7872eac851d4bd6db915
NgoPhuocDong <dongatdong@gmail.com>
2023-03-31 10:21:29 +0700

    Initial commit

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+    /* Tên cơ sở dữ liệu */
+    define('DB_NAME', 'nameDB');
+    /* Tài khoản để kết nối với cơ sở dữ liệu MySQL */
+    define('DB_USER', 'root');
+    /* Mật khẩu của tài khoản kết nối với cơ sở dữ liệu MySQL */
+    define('DB_PASSWORD', '');
+    /* Máy chủ cơ sở dữ liệu MySQL */
+    define('DB_HOST', 'localhost');
+?>
