9329405e24b3564a78dce56e2775a25feb4aaafe
NgoPhuocDong <dongatdong@gmail.com>
2023-04-12 21:31:12 +0700

    .

--- index.php
+++ index.php
@@ -10,5 +10,15 @@
     $controller = new $nameController();
     $controller->$action();
 
-    
+     //gọi hàm kết nối sql(connect) từ class Database trong Models
+     include "./Models/Database.php";
+     $db =  new Database();
+     $db->connect();
+ 
+     $nameController = ucfirst(($_REQUEST['url'] ?? 'Home').'Controller');
+     $action = $_REQUEST['action'] ?? 'TrangChu';
+     include "./Controllers/$nameController.php";
+     $controller = new $nameController();
+     $controller->$action();
+
 ?>
