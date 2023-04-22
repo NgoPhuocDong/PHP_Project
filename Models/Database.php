ad446ba9cb90cd6e7b3ede817b4721d09982f98a
NgoPhuocDong <dongatdong@gmail.com>
2023-04-22 16:04:02 +0700

    Revert "Revert "Update Database.php""
    This reverts commit ec870a9185a39697be93fe4787ee43f7cc3a91ad.

--- Models/Database.php
+++ Models/Database.php
@@ -3,7 +3,7 @@ class Database{
     private $host="localhost";
     private $username="root";
     private $password="";
-    private $database="quanlymaytinhmoi";
+    private $database="quanlymaytinh";
     private $conn;
     private $result;
 
