5615ed0dd0acb577abb2ebf619693115945a2093
phuclocdh <phuclocdh2017@gmail.com>
2023-05-10 14:56:13 +0700

    10/5

--- Controllers/DonHangMuaController.php
+++ Controllers/DonHangMuaController.php
@@ -1,13 +1,20 @@
 <?php
 include_once("Models/DonHangMua.php");
+include_once("Models/NguonHang.php");
+include_once("Models/NhanVien.php");
 
 class DonHangMuaController{
     private $model;
     private $db;
+    private $nguonhang;
+    private $nhanvienlap;
+    
     
     public function __construct(){
         $this->model = new DonHangMua();
         $this->db = new Database();
+        $this->nguonhang = new NguonHang();
+        $this->nhanvienlap = new NhanVien();
     }
     
     public function DanhSach()
@@ -35,23 +42,40 @@ class DonHangMuaController{
         return $result;
     }
     public function ThemMoi(){
+        $alert = "";
+        $ListNguonHang = $this->nguonhang->GetData(100,0);
+        $ListNhanVien = $this->nhanvienlap->DanhSach(100,0);
         if (isset($_POST['submit'])) {
+            if(empty($_POST['idnguonhang']) || empty($_POST['idnhanvienlap'])){
+                $alert="<span style='color: red; padding-bottom: 10px; display: block;'>Không được bỏ trống id nhân viên lập và nguồn hàng!</span>";
+            }else if(!is_numeric($_POST['idnguonhang']) || !is_numeric($_POST['idnhanvienlap'])){
+                $alert = "<span style='color: red; padding-bottom: 10px; display: block;'>id nhân viên và nguồn hàng bắt buộc phải là số!</span>";
+            }else{
             $create = $this->model->ThemMoi($_POST['idnhanvienlap'], $_POST['idnguonhang'],  $_POST['idtrangthai'],$_POST['ngaylap'],$_POST['tongtien']);
             if ($create) {
                 header('Location: ./DanhSach');
             }
         }
+        }
         include 'Views/DonHangMua/ThemMoi.php';
+        return Array($ListNguonHang,$ListNhanVien);
     }
 
     public function CapNhat(){
+        $alert = "";
         if (isset($_GET['id'])) {
             $id = $_GET['id'];
             $table = 'donhangmua';
             //lấy dữ liệu cần cập nhật
             $dataUpdate = $this->db->find($table,$id);
-            
+            $ListNguonHang = $this->nguonhang->GetData(100,0);
+            $ListNhanVien = $this->nhanvienlap->DanhSach(100,0);
             if (isset($_POST['submit'])) {
+                if(empty($_POST['idnguonhang']) || empty($_POST['idnhanvienlap'])){
+                    $alert="<span style='color: red; padding-bottom: 10px; display: block;'>Không được bỏ trống id nhân viên lập và nguồn hàng!</span>";
+                }else if(!is_numeric($_POST['idnguonhang']) || !is_numeric($_POST['idnhanvienlap'])){
+                    $alert = "<span style='color: red; padding-bottom: 10px; display: block;'>id nhân viên và nguồn hàng bắt buộc phải là số!</span>";
+                }else{
                 $update = $this->model->CapNhat($id,$_POST['idnhanvienlap'],
                                                     $_POST['idnguonhang'],
                                                     $_POST['idtrangthai'],
@@ -61,9 +85,10 @@ class DonHangMuaController{
                     header('Location: ./DanhSach');
                 }
             }
+            }
         }
         include 'Views/DonHangMua/CapNhat.php';
-        return $dataUpdate;
+        return Array($dataUpdate,$ListNguonHang,$ListNhanVien);
     }
 
     public function Xoa(){
